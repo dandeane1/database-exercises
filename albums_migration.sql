49d669b0d866794173f6d134679ca3a1848077af
Daniel Deane <daniel.michael.deane@gmail.com>
2023-10-23 12:18:46 -0500

    Added default null on all

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@
 USE codeup_test2_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist_name VARCHAR(50),
-                        record_name  VARCHAR(100) NOT NULL,
-                        release_date INT(4),
-                        sales DECIMAL(3,1),
-                        genre CHAR(50),
+                        id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist_name VARCHAR(50) DEFAULT NULL,
+                        record_name  VARCHAR(100) DEFAULT NULL,
+                        release_date INT(4) DEFAULT NULL,
+                        sales DECIMAL(3,1) DEFAULT NULL,
+                        genre CHAR(50) DEFAULT NULL,
                         PRIMARY KEY (id)
 );
