f4117a6f9791eb03a459523b9cd955a3f6f570ae
Daniel Deane <daniel.michael.deane@gmail.com>
2023-10-25 14:34:42 -0500

    group_by done

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 # Modify your first query to order by first name. The first result should be Irena Flexer and the last result should be Vidya Awdeh.
-SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER by first_name, last_name;
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER by first_name, last_name  ;
 
 
 # Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
